835ee162cb444f10fe6efa43f84c5fa660ee441a
An Nguyen <hitanan@gmail.com>
2025-04-10 03:35:07 +0000

    feat: Step 3: Initialize and create the octofit_db MongoDB database, Django project/app, update Django project/app files, and populate the MongoDB database (2)

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# Configure database (default SQLite for now)
+# Configure MongoDB database connection
 DATABASES = {
     'default': {
         'ENGINE': 'djongo',
